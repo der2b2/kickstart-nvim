341e4ef70bb1287712987f2f8984207f4f3f518e
Bastian Schroeder <bschroeder@Mac-mini-von-Bastian.fritz.box>
2025-09-25 17:55:38 +0200

    automatic

--- init.lua
+++ init.lua
@@ -177,6 +177,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('n', 'ge', ':$<Enter>', { desc = 'go to last line' })
+vim.keymap.set('v', '<leader>y', '"*y', { desc = 'Copy to Master Clipboard' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
